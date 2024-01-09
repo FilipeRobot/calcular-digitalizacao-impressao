ec2c59f20231a9f70f19ef5d39318a040f9a848f
Filipe Lemos <filipe_lemos@live.com>
2024-01-09 17:30:26 -0300

    ✨ feat(funcionalidade): melhorar script
    Praticamente todo o script da pagina foi refeito, qualquer mudança nos valores podem ser feitas mudando o objeto "valores"
    
    Esse script ainda pode ser melhorado, uma das ideias de melhoria é usar classes

--- js/script.js
+++ js/script.js
@@ -1,90 +1,156 @@
-function checkedBox(checkboxElement) {
-    if (checkboxElement.name === 'digitalizar') {
-        quantidadeDigitalizarElement = document.getElementById('quantidade-digitalizar');
-        let isHidden = quantidadeDigitalizarElement.getAttribute('hidden');
-        
-        cleanQuantidadeDigitalizar();
-        
-        if (isHidden === null) {
-            quantidadeDigitalizarElement.setAttribute('hidden', 'hidden');
-        } else {
-            quantidadeDigitalizarElement.removeAttribute('hidden');
-        }
-    }
-    
-    if (checkboxElement.name === 'imprimir') {
-        quantidadeImprimirElement = document.getElementById('quantidade-imprimir');
-        let isHidden = quantidadeImprimirElement.getAttribute('hidden');
-        
-        cleanQuantidadeImprimir()
-        
-        if (isHidden === null) {
-            quantidadeImprimirElement.setAttribute('hidden', 'hidden');
-        } else {
-            quantidadeImprimirElement.removeAttribute('hidden');
-        }
-    }
+const btnCalcular = document.querySelector('#btn_calcular');
+
+const valores = {
+	digitalizar: {
+		documento: 0.5,
+		folhaEncadernada: 0.3,
+		papelComum: 0.2,
+	},
+	imprimir: {
+		pretoBranco: 0.5,
+		colorido: 1,
+	},
+};
+
+const user = {
+	digitalizar: {
+		qtdDocumento: 0,
+		qtdFolhaEncadernada: 0,
+		qtdPapelComum: 0,
+	},
+	imprimir: {
+		qtdPretoBranco: 1,
+		qtdColorido: 0,
+	},
+};
+
+const formCustos = document.querySelector('#form_custos');
+
+const input = {
+	qtdDocumento: formCustos.qtd_documento,
+	qtdFolhaEncadernada: formCustos.qtd_folha_encadernada,
+	qtdPapelComum: formCustos.qtd_papel_comum,
+	qtdPretoBranco: formCustos.qtd_preto_branco,
+	qtdColorido: formCustos.qtd_colorido,
+};
+
+// Validar input do usuário no momento em que ele muda o valor do input
+for (const key in input) {
+	if (Object.hasOwnProperty.call(input, key)) {
+		const element = input[key];
+		element.addEventListener('change', (event) => {
+			/**
+			 *  Validar se a quantidade que o usuário quer é menor que 0 (zero)
+			 */
+			if (Number.parseInt(event.srcElement.value) < 0) {
+				event.srcElement.classList.add('erro');
+				event.srcElement.value = 0;
+				return;
+			}
+
+			/**
+			 * Validar se a quantidade que o usuário quer é maior que 50 (cinquenta)
+			 */
+			if (Number.parseInt(event.srcElement.value) > 50) {
+				event.srcElement.classList.add('erro');
+				event.srcElement.value = 50;
+				return;
+			}
+
+			event.srcElement.classList.remove('erro');
+		});
+	}
 }
 
-function cleanQuantidadeDigitalizar() {
-    let qtdDocumentos = document.getElementById('documento-quantidade');
-    let qtdPapelEncadernada = document.getElementById('folha-encadernada-quantidade');
-    let qtdPapelComum = document.getElementById('papel-comum-quantidade');
-    qtdDocumentos.value = 0;
-    qtdPapelEncadernada.value = 0;
-    qtdPapelComum.value = 0;
-}
+/**
+ * Executar o calculo do custo total de digitalização e impressão
+ * com base na quantidade de tipos de digitalização e impressão que
+ * o usuário selecionou no formulário.
+ */
+btnCalcular.addEventListener('click', (event) => {
+	event.preventDefault();
 
-function cleanQuantidadeImprimir() {
-    let qtdPretoBranco = document.getElementById('preto-branco-quantidade');
-    let qtdColorido = document.getElementById('colorido-quantidade');
-    qtdPretoBranco.value = 0;
-    qtdColorido.value = 0;
-}
+	const precisaDigitalizar = formCustos.digitalizar.checked;
+	const precisaImprimir = formCustos.imprimir.checked;
 
-function calcular() {
-    const qtdDocumentos = document.getElementById('documento-quantidade').value;
-    const qtdPapelEncadernada = document.getElementById('folha-encadernada-quantidade').value;
-    const qtdPapelComum = document.getElementById('papel-comum-quantidade').value;
-    
-    const qtdPretoBranco = document.getElementById('preto-branco-quantidade').value;
-    const qtdColorido = document.getElementById('colorido-quantidade').value;
-    
-    const valorDocumento = 0.50;
-    const valorPapelEncar = 0.30;
-    const valorPapelComum = 0.20;
-    
-    const valorPretoBranco = 0.50;
-    const valorColorido = 1;
-    
-    let valor = 0;
-    
-    valor += qtdDocumentos * valorDocumento;
-    valor += qtdPapelEncadernada * valorPapelEncar;
-    valor += qtdPapelComum * valorPapelComum;
-    
-    valor += qtdPretoBranco * valorPretoBranco;
-    valor += qtdColorido * valorColorido;
-    
-    console.log(valor);
-    
-    return valor;
-}
+	let valor = 0;
 
-function formatPreco(valor) {
-    const BRReal = new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-    });
-    return BRReal.format(valor);
-}
+	if (precisaDigitalizar) {
+		user.digitalizar = {
+			qtdDocumento: Number.parseInt(input.qtdDocumento.value),
+			qtdFolhaEncadernada: Number.parseInt(
+				input.qtdFolhaEncadernada.value
+			),
+			qtdPapelComum: Number.parseInt(input.qtdPapelComum.value),
+		};
 
-function exibirValor() {
-    let valorCalculadoElement = document.getElementById('valor-calculado');
-    
-    let valor = calcular();
-    
-    const valorFormatado = formatPreco(valor);
-    
-    valorCalculadoElement.innerHTML = valorFormatado;
+		valor += user.digitalizar.qtdDocumento * valores.digitalizar.documento;
+		valor +=
+			user.digitalizar.qtdFolhaEncadernada *
+			valores.digitalizar.folhaEncadernada;
+		valor +=
+			user.digitalizar.qtdPapelComum * valores.digitalizar.papelComum;
+	}
+
+	if (precisaImprimir) {
+		user.imprimir = {
+			qtdPretoBranco: Number.parseInt(input.qtdPretoBranco.value),
+			qtdColorido: Number.parseInt(input.qtdColorido.value),
+		};
+
+		valor += user.imprimir.qtdPretoBranco * valores.imprimir.pretoBranco;
+		valor += user.imprimir.qtdColorido * valores.imprimir.colorido;
+	}
+
+	mostrarResultado(valor);
+});
+
+const checkboxes = formCustos.querySelectorAll('.__input--check');
+
+checkboxes.forEach((checkBox) => {
+	checkBox.addEventListener('click', (event) => {
+		const idDigitalizar = 'digitalizar';
+		const idImprimir = 'imprimir';
+		const eventId = event.target.id;
+
+		if (eventId === idDigitalizar) {
+			const qtdDigitalizarDiv =
+				event.target.ownerDocument.querySelector('#qtd_digitalizar');
+			const isHidden = qtdDigitalizarDiv.getAttribute('hidden');
+
+			input.qtdDocumento.value = 0;
+			input.qtdFolhaEncadernada.value = 0;
+			input.qtdPapelComum.value = 0;
+
+			if (isHidden === null) {
+				qtdDigitalizarDiv.setAttribute('hidden', 'hidden');
+			} else {
+				qtdDigitalizarDiv.removeAttribute('hidden');
+			}
+		}
+
+		if (eventId === idImprimir) {
+			const qtdImprimirDiv =
+				event.target.ownerDocument.querySelector('#qtd_digitalizar');
+			const isHidden = qtdImprimirDiv.getAttribute('hidden');
+
+			input.qtdPretoBranco.value = 0;
+			input.qtdColorido.value = 0;
+
+			if (isHidden === null) {
+				qtdImprimirDiv.setAttribute('hidden', 'hidden');
+			} else {
+				qtdImprimirDiv.removeAttribute('hidden');
+			}
+		}
+	});
+});
+
+function mostrarResultado(valor) {
+	const resultado = document.querySelector('#valor_calculado');
+	const BRReal = new Intl.NumberFormat('pt-BR', {
+		style: 'currency',
+		currency: 'BRL',
+	});
+	resultado.innerHTML = BRReal.format(valor);
 }
